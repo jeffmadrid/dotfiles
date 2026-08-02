478226164d8980407d45d62c20f9fd759c9916a6
Jefferson Madrid <jefferson.imadrid@gmail.com>
2026-08-02 13:49:46 +1000

    diffview

---
+++ nvim/.config/nvim/lua/custom/plugins/diffview.lua
@@ -0,0 +1,35 @@
+-- Diffview - git diff and file history in a tabbed UI
+-- https://github.com/sindrets/diffview.nvim
+
+vim.pack.add {
+  'https://github.com/sindrets/diffview.nvim',
+  'https://github.com/nvim-lua/plenary.nvim',
+}
+
+require('diffview').setup {}
+
+-- Open the diff for the current change (falls back to current buffer)
+vim.keymap.set('n', '<leader>gd', '<Cmd>DiffviewOpen<CR>', { desc = 'Diffview open' })
+
+-- Git history of the current file
+vim.keymap.set('n', '<leader>gh', '<Cmd>DiffviewFileHistory %<CR>', { desc = 'Diffview file history' })
+
+-- Diff HEAD against the default branch (origin/HEAD, falling back to main/master)
+vim.keymap.set('n', '<leader>gb', function()
+  local remote_head = vim.trim(vim.fn.system { 'git', 'symbolic-ref', 'refs/remotes/origin/HEAD' })
+  local branch = vim.v.shell_error == 0 and remote_head:match 'refs/remotes/origin/(.+)' or nil
+  if not branch then
+    for _, name in ipairs { 'main', 'master' } do
+      local ok = vim.fn.system { 'git', 'rev-parse', '--verify', '--quiet', 'refs/heads/' .. name }
+      if vim.v.shell_error == 0 and ok ~= '' then
+        branch = name
+        break
+      end
+    end
+  end
+  if not branch then
+    vim.notify('Could not determine default branch', vim.log.levels.ERROR)
+    return
+  end
+  vim.cmd('DiffviewOpen HEAD...' .. branch)
+end, { desc = 'Diffview: compare HEAD to default branch' })
